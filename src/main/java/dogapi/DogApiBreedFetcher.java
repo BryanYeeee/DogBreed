5d2426abf30e42f165095b4f3c2e6620e50e672c
BryanYeeee <haloday873@gmail.com>
2025-10-16 18:36:33 -0400

    dogbreed error

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -28,7 +28,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException{
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
@@ -52,7 +52,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
             else {
                 throw new BreedNotFoundException(breed);
             }
-        } catch (IOException | BreedNotFoundException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return subBreedsList;
